6503d310d8817fc25a1f4899ea13a9de00d70eae
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-04-17 22:34:42 +0530

    header layout is done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from '../components/Header'
 import './globals.css'
 
 export const metadata = {
@@ -8,7 +9,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+          <Header />
+        {/* Navbar */}
+
+        {/* SearchBar */}
+
+        {children}
+
+      </body>
     </html>
   )
 }
